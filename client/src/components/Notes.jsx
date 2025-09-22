949312bc1e0cc59f1bff652810cc0d99eccb08b5
Eric Musembi <ericmusembi2012@gmail.com>
2025-09-22 20:12:28 +0300

    update client

---
+++ client/src/components/Notes.jsx
@@ -0,0 +1,10 @@
+/*
+useState is a hook provided by react that allows you to add state to functional components. it returns an array with exactly two elements:
+The current state value 
+
+Create a react+vite project, give it a name of your choice, then use bootstrap for your work.
+Create a card for your products, then create data.js to store your data and get all the products 
+in the data.js to populate your card. 
+Add interactivity to your app, and use props to pass the product data to cards.
+
+ */
